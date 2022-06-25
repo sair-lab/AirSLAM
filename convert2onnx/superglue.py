93b67b838739aab75a982c33bb96fde3338063b8
yuefanhao <yuefan.hao@outlook.com>
2022-06-25 13:45:29 +0800

    tensorrt 8.4 fixed

--- convert2onnx/superglue.py
+++ convert2onnx/superglue.py
@@ -173,36 +173,36 @@ class AttentionalGNN(nn.Module):
         return desc0, desc1
 
 
-# def log_sinkhorn_iterations(Z: torch.Tensor, log_mu: torch.Tensor, log_nu: torch.Tensor, iters: int) -> torch.Tensor:
-#     """ Perform Sinkhorn Normalization in Log-space for stability"""
-#     u, v = torch.zeros_like(log_mu), torch.zeros_like(log_nu)
-#     for _ in range(iters):
-#         u = log_mu - torch.logsumexp(Z + v.unsqueeze(1), dim=2)
-#         v = log_nu - torch.logsumexp(Z + u.unsqueeze(2), dim=1)
-#     return Z + u.unsqueeze(2) + v.unsqueeze(1)
+def log_sinkhorn_iterations(Z: torch.Tensor, log_mu: torch.Tensor, log_nu: torch.Tensor, iters: int) -> torch.Tensor:
+    """ Perform Sinkhorn Normalization in Log-space for stability"""
+    u, v = torch.zeros_like(log_mu), torch.zeros_like(log_nu)
+    for _ in range(iters):
+        u = log_mu - torch.logsumexp(Z + v.unsqueeze(1), dim=2)
+        v = log_nu - torch.logsumexp(Z + u.unsqueeze(2), dim=1)
+    return Z + u.unsqueeze(2) + v.unsqueeze(1)
 
 
-# def log_optimal_transport(scores: torch.Tensor, alpha: torch.Tensor, iters: int) -> torch.Tensor:
-#     """ Perform Differentiable Optimal Transport in Log-space for stability"""
-#     b, m, n = scores.shape
-#     one = scores.new_tensor(1)
-#     ms, ns = (m*one).to(scores), (n*one).to(scores)
-#
-#     bins0 = alpha.expand(b, m, 1)
-#     bins1 = alpha.expand(b, 1, n)
-#     alpha = alpha.expand(b, 1, 1)
-#
-#     couplings = torch.cat([torch.cat([scores, bins0], -1),
-#                            torch.cat([bins1, alpha], -1)], 1)
-#
-#     norm = - (ms + ns).log()
-#     log_mu = torch.cat([norm.expand(m), ns.log()[None] + norm])
-#     log_nu = torch.cat([norm.expand(n), ms.log()[None] + norm])
-#     log_mu, log_nu = log_mu[None].expand(b, -1), log_nu[None].expand(b, -1)
-#
-#     Z = log_sinkhorn_iterations(couplings, log_mu, log_nu, iters)
-#     Z = Z - norm  # multiply probabilities by M+N
-#     return Z
+def log_optimal_transport(scores: torch.Tensor, alpha: torch.Tensor, iters: int) -> torch.Tensor:
+    """ Perform Differentiable Optimal Transport in Log-space for stability"""
+    b, m, n = scores.shape
+    one = scores.new_tensor(1)
+    ms, ns = (m*one).to(scores), (n*one).to(scores)
+
+    bins0 = alpha.expand(b, m, 1)
+    bins1 = alpha.expand(b, 1, n)
+    alpha = alpha.expand(b, 1, 1)
+
+    couplings = torch.cat([torch.cat([scores, bins0], -1),
+                           torch.cat([bins1, alpha], -1)], 1)
+
+    norm = - (ms + ns).log()
+    log_mu = torch.cat([norm.expand(m), ns.log()[None] + norm])
+    log_nu = torch.cat([norm.expand(n), ms.log()[None] + norm])
+    log_mu, log_nu = log_mu[None].expand(b, -1), log_nu[None].expand(b, -1)
+
+    Z = log_sinkhorn_iterations(couplings, log_mu, log_nu, iters)
+    Z = Z - norm  # multiply probabilities by M+N
+    return Z
 
 
 # def arange_like(x, dim: int):
