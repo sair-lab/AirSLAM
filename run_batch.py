e00bc6da7ba64a92386ab6af417ea1ca26b180a7
xukuanHIT <403282859@qq.com>
2022-08-29 08:59:38 +0800

    debug

--- run_batch.py
+++ run_batch.py
@@ -22,23 +22,22 @@ MakeDir(eva_sum_root)
 
 os.system("cd {} & roscore & .".format(workspace))
 sequences = os.listdir(dataroot)
-# for sequence in sequences:
-#   seq_dataroot = os.path.join(dataroot, sequence)
-#   seq_traj_file = sequence + ".txt"
-#   seq_traj_path = os.path.join(traj_saving_root, seq_traj_file)
-#   os.system("cd {} & roslaunch air_vo euroc.launch dataroot:={} traj_path:={}".format(workspace, seq_dataroot, seq_traj_path))
+for sequence in sequences:
+  seq_dataroot = os.path.join(dataroot, sequence)
+  seq_traj_file = sequence + ".txt"
+  seq_traj_path = os.path.join(traj_saving_root, seq_traj_file)
+  os.system("cd {} & roslaunch air_vo euroc.launch dataroot:={} traj_path:={}".format(workspace, seq_dataroot, seq_traj_path))
 
-#   gt_path = os.path.join(traj_gt_dir, seq_traj_file)
-#   if not os.path.exists(gt_path):
-#     csv_gt_file = sequence + ".csv"
-#     gt_path = os.path.join(traj_gt_dir, csv_gt_file)
-#     if not os.path.exists(gt_path):
-#       continue
+  gt_path = os.path.join(traj_gt_dir, seq_traj_file)
+  if not os.path.exists(gt_path):
+    csv_gt_file = sequence + ".csv"
+    gt_path = os.path.join(traj_gt_dir, csv_gt_file)
+    if not os.path.exists(gt_path):
+      continue
 
-  # eva_seq_file = sequence + ".zip"
-  # eva_seq_path = os.path.join(eva_seq_root, eva_seq_file)
-  # os.system("evo_ape tum {} {} -a --save_results {}".format(gt_path, seq_traj_path, eva_seq_path))
+  eva_seq_file = sequence + ".zip"
+  eva_seq_path = os.path.join(eva_seq_root, eva_seq_file)
+  os.system("evo_ape tum {} {} -a --save_results {}".format(gt_path, seq_traj_path, eva_seq_path))
 
 table_file = os.path.join(eva_sum_root, "sum.csv")
-print(eva_seq_root)
-os.system("cd {} & evo_res *.zip -p --save_table {}".format(eva_seq_root, table_file))
+os.system("evo_res {}/*.zip -p --save_table {}".format(eva_seq_root, table_file))
