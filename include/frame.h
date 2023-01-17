f2d09dd5c2608bbdf50b2388a36d6b68ee08926e
xukuanHIT <403282859@qq.com>
2023-01-17 16:54:37 +0800

    split left and right features

--- include/frame.h
+++ include/frame.h
@@ -33,6 +33,9 @@ public:
   void AddFeatures(Eigen::Matrix<double, 259, Eigen::Dynamic>& features_left, 
       Eigen::Matrix<double, 259, Eigen::Dynamic>& features_right, std::vector<Eigen::Vector4d>& lines_left, 
       std::vector<Eigen::Vector4d>& lines_right, std::vector<cv::DMatch>& stereo_matches);
+  void AddLeftFeatures(Eigen::Matrix<double, 259, Eigen::Dynamic>& features_left, std::vector<Eigen::Vector4d>& lines_left);
+  void AddRightFeatures(Eigen::Matrix<double, 259, Eigen::Dynamic>& features_right, std::vector<Eigen::Vector4d>& lines_right, std::vector<cv::DMatch>& stereo_matches);
+
   Eigen::Matrix<double, 259, Eigen::Dynamic>& GetAllFeatures();
 
   size_t FeatureNum();
